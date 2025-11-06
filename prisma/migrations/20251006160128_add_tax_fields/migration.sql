a68c963edec8b923508119d513d14495c8a56fa6
Jev Belovs <ilguciems@gmail.com>
2025-11-06 21:06:56 +0100

    chore: Reset prisma migrations

--- prisma/migrations/20251006160128_add_tax_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Company" ADD COLUMN     "firstTaxRate" INTEGER DEFAULT 19,
-ADD COLUMN     "isSubjectToVAT" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "secondTaxRate" INTEGER DEFAULT 7;
---
+++ prisma/migrations/20251105213343_init/migration.sql
@@ -0,0 +1,75 @@
+-- CreateEnum
+CREATE TYPE "public"."LegalForm" AS ENUM ('KLEINGEWERBE', 'FREIBERUFLER', 'GBR', 'EINZELKAUFMANN', 'OHG', 'KG', 'GMBH_CO_KG', 'GMBH', 'UG', 'AG', 'KGaA', 'SE', 'EWIV');
+
+-- CreateTable
+CREATE TABLE "public"."Company" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "houseNumber" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL DEFAULT 'Deutschland',
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "iban" TEXT NOT NULL,
+    "bic" TEXT NOT NULL,
+    "bank" TEXT NOT NULL,
+    "logoUrl" TEXT,
+    "isSubjectToVAT" BOOLEAN NOT NULL DEFAULT false,
+    "firstTaxRate" DOUBLE PRECISION,
+    "secondTaxRate" DOUBLE PRECISION,
+    "legalForm" "public"."LegalForm" NOT NULL DEFAULT 'KLEINGEWERBE',
+    "steuernummer" TEXT,
+    "ustId" TEXT,
+    "handelsregisternummer" TEXT,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Invoice" (
+    "id" SERIAL NOT NULL,
+    "invoiceNumber" TEXT NOT NULL,
+    "customerName" TEXT NOT NULL,
+    "customerStreet" TEXT NOT NULL,
+    "customerHouseNumber" TEXT NOT NULL,
+    "customerZipCode" TEXT NOT NULL,
+    "customerCity" TEXT NOT NULL,
+    "customerCountry" TEXT NOT NULL DEFAULT 'Deutschland',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isPaid" BOOLEAN NOT NULL DEFAULT false,
+    "paidAt" TIMESTAMP(3),
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Item" (
+    "id" SERIAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "unitPrice" DOUBLE PRECISION NOT NULL,
+    "taxRate" DOUBLE PRECISION,
+    "invoiceId" INTEGER NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Company_createdAt_idx" ON "public"."Company"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Company_updatedAt_idx" ON "public"."Company"("updatedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invoice_invoiceNumber_key" ON "public"."Invoice"("invoiceNumber");
+
+-- AddForeignKey
+ALTER TABLE "public"."Invoice" ADD CONSTRAINT "Invoice_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "public"."Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Item" ADD CONSTRAINT "Item_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "public"."Invoice"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
