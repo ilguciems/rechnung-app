c73aa19caee5f5446e41a969879fbf61e396a4a5
Jev Belovs <ilguciems@gmail.com>
2025-12-06 14:22:16 +0100

    fix: Adjust types

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -12,7 +12,7 @@ import type { Company, Invoice } from "@/lib/zod-schema";
 
 interface CreatedInvoice extends Invoice {
   invoiceNumber: string;
-  customerNumber: string;
+  customerNumber: string | null;
   createdAt: Date;
   isPaid: boolean;
   paidAt: Date | null;
