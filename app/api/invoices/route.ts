91aa3d14feeca261bc5967049887a6ebf2f5d1b7
Jev Belovs <ilguciems@gmail.com>
2025-12-17 14:33:08 +0100

    fix: Prisma type (#32)

--- app/api/invoices/route.ts
+++ app/api/invoices/route.ts
@@ -1,4 +1,4 @@
-import type { Prisma } from "@prisma/client";
+import type { Prisma } from "../../generated/prisma/client";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 import { prisma } from "@/lib/prisma-client";
