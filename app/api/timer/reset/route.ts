b6f88a5932f0765ff8ef0a602bc82b0b88dac395
Jev Belovs <ilguciems@gmail.com>
2025-12-28 22:45:16 +0100

    feat: Add constants for various logout timers

--- app/api/timer/reset/route.ts
+++ app/api/timer/reset/route.ts
@@ -1,12 +1,13 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const TWO_HOURS = 1000 * 60 * 60 * 2;
+export const SIX_MINUTES = 1000 * 60 * 6;
 export async function POST() {
-  const expiresAt = Date.now() + 1000 * 60 * 6; // + 1000 * 60 * 60 * 2; // +2 часа
+  const expiresAt = Date.now() + TWO_HOURS;
   (await cookies()).set("timer-expires-at", expiresAt.toString(), {
     httpOnly: true,
     secure: true,
   });
   return NextResponse.json({ success: true });
 }
-//+ 1000 * 60 * 6;
