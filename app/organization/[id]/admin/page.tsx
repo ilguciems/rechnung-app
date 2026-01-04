a1ba08b2d4935242fe4b3c7446002b99f7a23d90
Jev Belovs <ilguciems@gmail.com>
2026-01-04 15:52:23 +0100

    feat: Implement change org name and send invitation for org admin

--- app/organization/[id]/admin/page.tsx
+++ app/organization/[id]/admin/page.tsx
@@ -1,7 +1,22 @@
-export default function Admin() {
+import { redirect } from "next/navigation";
+import { getAuthData } from "@/lib/get-auth-data";
+import { NameForm, SendInviteForm } from "./components";
+
+export default async function Admin() {
+  const session = await getAuthData();
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  if (session.org?.role !== "admin") {
+    redirect("/");
+  }
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1>organization Admin</h1>
+    <div className="p-6 max-w-3xl mx-auto space-y-8">
+      <NameForm />
+      <SendInviteForm />
     </div>
   );
 }
