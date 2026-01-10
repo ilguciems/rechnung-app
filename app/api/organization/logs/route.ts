31799184ff4c1e330c37d8d88e0bcfc85a566a72
Jev Belovs <ilguciems@gmail.com>
2026-01-10 14:49:39 +0100

    feat: Add logs api route

---
+++ app/api/organization/logs/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { getAuthData } from "@/lib/get-auth-data";
+import { prisma } from "@/lib/prisma-client";
+
+export async function GET() {
+  const session = await getAuthData();
+
+  if (!session || !session.org || session.org.role !== "admin") {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const logs = await prisma.activityLog.findMany({
+    where: {
+      organizationId: session.org?.id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (logs) {
+    return NextResponse.json(logs);
+  }
+  return NextResponse.json([]);
+}
