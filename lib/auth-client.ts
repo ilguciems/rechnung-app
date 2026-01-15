11ca61e7387fa66bac74379c189b5d5ab0d51c8c
Jev Belovs <ilguciems@gmail.com>
2026-01-15 17:41:20 +0100

    feat: Add roles to auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { ac, admin as adminRole, superadmin, user } from "./permissions";
 
 export const {
   signIn,
@@ -13,6 +14,15 @@ export const {
   changeEmail,
   admin,
 } = createAuthClient({
-  plugins: [adminClient()],
+  plugins: [
+    adminClient({
+      ac,
+      roles: {
+        superadmin,
+        admin: adminRole,
+        user,
+      },
+    }),
+  ],
   baseURL: "http://localhost:3000",
 });
