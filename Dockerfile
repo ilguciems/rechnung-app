32cd768368cd008ba78ed970ab5ec76ff5bb4e7c
Jev Belovs <ilguciems@gmail.com>
2026-05-16 18:23:06 +0200

    fix: Adjust mail sender

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,17 @@
-# Use official Node.js image
-FROM node:24-alpine
-
-# Set working directory
+FROM node:24-alpine4.0.4
 WORKDIR /app
 
-# Install dependencies
 COPY package*.json ./
 RUN npm install
 
-# Copy source code
 COPY . .
 
 RUN DATABASE_URL="postgresql://fake:fake@localhost:5432/fake" npx prisma generate
 
-ARG NEXT_PUBLIC_APP_URL
-ENV NEXT_PUBLIC_APP_URL=$NEXT_PUBLIC_APP_URL
-
 ARG NEXT_PUBLIC_BASE_URL
 ENV NEXT_PUBLIC_BASE_URL=$NEXT_PUBLIC_BASE_URL
 
-# Build the application
-RUN MAILJET_API_KEY="fake" MAILJET_API_SECRET="fake" ABLY_API_KEY="fake" EMAIL_FROM="fake" npm run build
+RUN npm run build
 
 EXPOSE 3000
-
 CMD ["npm", "start"]
--- utils/authorization-email.ts
+++ utils/authorization-email.ts
@@ -1,12 +1,5 @@
 import Mailjet from "node-mailjet";
 
-const mailjet = Mailjet.apiConnect(
-  process.env.MAILJET_API_KEY as string,
-  process.env.MAILJET_API_SECRET as string,
-);
-
-const emailFrom = process.env.EMAIL_FROM as string;
-
 type SendEmailProps = {
   to: string;
   subject: string;
@@ -18,6 +11,17 @@ export async function sendAuthorizationEmail({
   subject,
   text,
 }: SendEmailProps) {
+  const apiKey = process.env.MAILJET_API_KEY;
+  const apiSecret = process.env.MAILJET_API_SECRET;
+  const emailFrom = process.env.EMAIL_FROM;
+
+  if (!apiKey || !apiSecret || !emailFrom) {
+    console.error("Mailjet configuration is missing in environment variables!");
+    throw new Error("Email service is misconfigured.");
+  }
+
+  const mailjet = Mailjet.apiConnect(apiKey, apiSecret);
+
   try {
     const response = await mailjet.post("send", { version: "v3.1" }).request({
       Messages: [
